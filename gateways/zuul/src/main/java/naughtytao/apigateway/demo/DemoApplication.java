ae5cf3e10c5d13b27c9152a6d1380791198c0cbb
Gang Tao <gangtao@Gangs-MacBook-Pro.local>
2021-03-19 12:43:57 -0700

    update

--- gateways/zuul/src/main/java/naughtytao/apigateway/demo/DemoApplication.java
+++ gateways/zuul/src/main/java/naughtytao/apigateway/demo/DemoApplication.java
@@ -22,21 +22,4 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-    // @Bean
-	// public PreFilter preFilter() {
-	// 	return new PreFilter();
-	// }
-	// @Bean
-	// public PostFilter postFilter() {
-	// 	return new PostFilter();
-	// }
-	// @Bean
-	// public ErrorFilter errorFilter() {
-	// 	return new ErrorFilter();
-	// }
-	// @Bean
-	// public RouteFilter routeFilter() {
-	// 	return new RouteFilter();
-	// }
 }
